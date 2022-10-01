78a6efd35386f8f52d3c1c7e614996306d0f3e5f
Fiterman <gfiterman96@gmail.com>
2022-10-01 17:35:58 -0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>{{ test }}</h1>
+  </main>
+</template>
+
+<script>
+  
+export default {
+  name: 'Home',
+  data(){
+    return {
+      test: 'Initial value',
+    };
+  },
+  created(){
+    this.test = import.meta.env.VITE_TEST_ENV;
+  }
+}
+</script>
