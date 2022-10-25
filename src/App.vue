f9cc06408ef33cc8801d66df564c24820c6cd5ea
GabFiterman <gfiterman96@gmail.com>
2022-10-25 12:19:33 -0300

    style: Add minimal styling, images and fonts

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,55 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&family=Righteous&display=swap');
 
-body{
-  background-color: #252525;
+$color__text-main: #FFD3D3;
+
+$font__titles: 'Righteous', cursive;
+$font__main: 'Comfortaa', cursive;
+
+body {
+  background-image: url('../public/Main_Background.webp');
+  background-repeat: repeat;
+  background-size: cover;
+
   color: #fafafa;
-  font-family: 'poppins',  Arial, Helvetica, sans-serif;
+
+  
+  font-family: $font__main;
+  font-size: 16px;
+
+  h1 {
+    font-size: 2.5rem;
+    font-family: $font__titles;
+  }
+
+  h2 {
+    font-size: 2rem;
+    font-family: $font__titles;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    font-family: $font__titles;
+  }
+
+  p {
+    font-family: $font__main;
+    font-size: 1rem;
+
+    .sm {
+      font-size: 0.75rem;
+    }
+
+    .xsm {
+      font-size: 0.5rem;
+    }
+  }
+
+  b, strong {
+    font-weight: bold;
+  }
+
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <main v-if="API_responses">
 
-    <div class="API_card" v-for="(res, index) in API_responses" :key="index">
+    <!-- <div class="API_card" v-for="(res, index) in API_responses" :key="index">
       <h1>{{ res.name }}</h1>
       <img :src="res.thumbnail.path + '.' + res.thumbnail.extension" />
-    </div>
-
+    </div> -->
+    <h1>Comics Now</h1>
 
   </main>
 </template>
@@ -42,7 +42,7 @@ export default {
 
     this.API_fetchType = 'characters';
     this.API_fetchFilter = 'nameStartsWith';
-    this.API_fetchParam = 'Loki'
+    this.API_fetchParam = 'Black'
 
   },
 
@@ -67,27 +67,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.API_card{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 40vw;
-  margin: 5vh 25vw;
-  padding: 2rem 0 6rem 0;
-  background-color: rgba(255, 255, 255, 0.45);
-  border-radius: 1.2rem;
+// .API_card{
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   width: 40vw;
+//   margin: 5vh 25vw;
+//   padding: 2rem 0 6rem 0;
+//   background-color: rgba(255, 255, 255, 0.45);
+//   border-radius: 1.2rem;
 
-  h1 {
-    text-align: center;
-    font-size: 2.5rem;
-    max-width: 30vw;
-  }
+//   h1 {
+//     text-align: center;
+//     font-size: 2.5rem;
+//     max-width: 30vw;
+//   }
 
-  img{
-    max-height: 75vh;
-    border: 2px solid white;
-    border-radius: 1.2rem;
-  }
-}
+//   img{
+//     max-height: 75vh;
+//     border: 2px solid white;
+//     border-radius: 1.2rem;
+//   }
+// }
 </style>
